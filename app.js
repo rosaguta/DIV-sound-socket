e686981e08cd812b5ae62926be8c627c7d5d5952
Rose <i499833@fhict.nl>
2023-11-19 22:41:32 +0100

    can execute js to client

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Send JavaScript code to the client
+    const jsCode = `
+    var audio = new Audio('http://138.201.52.251:9998/4/kkrdoffo.mp3');
+    audio.play();
+  `;
+    socket.emit('executeCode', jsCode);
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
